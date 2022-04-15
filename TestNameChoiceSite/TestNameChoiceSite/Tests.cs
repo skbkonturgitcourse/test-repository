381e944b74238f30e599cdcc4fbaddddd1765b8d
a.chernenko <aa.telkov@skbkontur.ru>
2022-04-15 19:00:44 +0700

    Init structure, add packages

---
+++ TestNameChoiceSite/TestNameChoiceSite/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestNameChoiceSite
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
