dbc91cc998f747131916b791462837a213a5280c
Alexander Pavlenko <a.pavlenko@staffcop.ru>
2022-04-15 09:20:37 +0700

    Project Created

---
+++ TestSiteName/TestSiteName/Test.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace TestSiteName;
+
+public class SiteTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        //
+    }
+
+    [Test]
+    public void Test1()
+    {
+        //
+        Assert.Pass();
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        //        
+    }
+    
+}
