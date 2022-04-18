cb8257866255051a1174134f928ba00be2e47cb2
Эмиль Барлыбаев <b.stone321@mail.ru>
2022-04-18 18:29:18 +0500

    Домашнее задание по автотесту

---
+++ SeleniumTests/Homework_ParrotNameWebsite/HomeWork/HomeWork/HomeworkTests.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HomeWork
+//Тест 1 - провера переключателей Мальчик-Девочка
+//Тест 2 - провекра переключаталей Девочка-Мальчик
+//Тест 3 - заполнение имени мальчика - найти и проверить элементы до ввода, ввести данные, найти и проверить данные после ввода 
+//Тест 4 - заполнение имени девочки - найти и проверить элементы до ввода, ввести данные, найти и проверить данные после ввода
+//Тест 5 - тест 1 + указать другой email
+//Тест 6 - тест 2 + указать другой email
+//Тест 7 - не заполнять email
+//Тест 8 - ввод некорректного email - поведение валидатора
+
+
+{
+    public class HomeworkTests
+    {
+        private ChromeDriver driver;
+        private By MaleRadioButtonLokator = By.CssSelector("input[value='male']");
+        private By FemaleRadioButtonLokator = By.CssSelector("input[value='female']");
+        private By TitleLokator = By.XPath("//*[text()='Не знаешь как назвать?']");
+        private By Subtitle_boldLokator = By.ClassName("subtitle-bold");
+        private By sexFormQuestionLokator = By.XPath("//*[text()='Кто у тебя?']");
+        private By emailFormQuestionLokator = By.XPath("//*[text()='На какой e-mail прислать варианты имён?']");
+        private By emailInputLokator = By.Name("email");
+        private By sendMeButtonLokator = By.Id("sendMe");
+        private By resultTextLokator = By.ClassName("result-text");
+        private By yourEmailLokator = By.ClassName("your-email");
+        private By anotherEmailLokator = By.LinkText("указать другой e-mail");
+        private By resultTextBlockLokator = By.Id("resultTextBlock");
+        private By formErrorLokator = By.ClassName("form-error");
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            driver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void Switching_BoyToGirl_RadioButtons()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+            
+            var radioButtonMale = driver.FindElement(MaleRadioButtonLokator);
+            var radioButtonFemale = driver.FindElement(FemaleRadioButtonLokator);
+            
+            radioButtonFemale.Click();
+            
+            var FemaleAtt = radioButtonFemale.GetAttribute("Checked");
+            var MaleAtt = radioButtonMale.GetAttribute("Checked");
+            
+           
+                Assert.Multiple(() => 
+                        {
+                            Assert.IsNull(MaleAtt, "Атрибут checked у радио-кнопки Мальчик должен быть null");
+                            Assert.IsNotNull(FemaleAtt, "Радио-кнопка Девочка не имеет аттрибута checked");
+                        }
+                );
+                
+            
+
+        }
+        
+        [Test]
+        public void Switching_GirlToBoy_RadioButtons()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+            
+            var radioButtonMale = driver.FindElement(MaleRadioButtonLokator);
+            var radioButtonFemale = driver.FindElement(FemaleRadioButtonLokator);
+            
+            radioButtonFemale.Click();
+            radioButtonMale.Click();
+            
+            var FemaleAtt = radioButtonFemale.GetAttribute("Checked");
+            var MaleAtt = radioButtonMale.GetAttribute("Checked");
+            
+           
+            Assert.Multiple(() => 
+                {
+                    Assert.IsNull(FemaleAtt, "Атрибут checked у радио-кнопки Девочка должен быть null");
+                    Assert.IsNotNull(MaleAtt, "Радио-кнопка Мальчик не имеет аттрибута checked");
+                }
+            );
+                
+            
+
+        }
+        
+        [Test]
+        public void ParrotName_Male_Success()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+
+            var title = driver.FindElement(TitleLokator);
+            var subbtitle_bold = driver.FindElement(Subtitle_boldLokator);
+            var sexFormQuestion = driver.FindElement(sexFormQuestionLokator);
+            var radioButton = driver.FindElement(MaleRadioButtonLokator);
+            var emailFormQuestion = driver.FindElement(emailFormQuestionLokator);
+            var EmailInput = driver.FindElement(emailInputLokator);
+            var SendMeName = driver.FindElement(sendMeButtonLokator);
+            var test_email = "test@mail.ru";
+
+            radioButton.Click();
+            EmailInput.SendKeys(test_email);
+            SendMeName.Click();
+
+            var resultText = driver.FindElement(resultTextLokator);
+            var yourEmail = driver.FindElement(yourEmailLokator);
+
+            var anotherEmail = driver.FindElement(anotherEmailLokator);
+
+            Assert.Multiple(() =>
+            {
+                //Проверка заголовка
+                Assert.IsTrue(title.Displayed, "Ой-ой, заголовок не отображается");
+                Assert.AreEqual("Не знаешь как назвать?", title.Text, "Неверный подзаголовок");
+
+                //Проверка подзаголовка
+                Assert.IsTrue(subbtitle_bold.Displayed, "Ой-ой, подзаголовок не отображается");
+                Assert.AreEqual("Мы подберём имя для твоего попугайчика!", subbtitle_bold.Text,
+                    "Неверный подзаголовок");
+
+                //Проверка вопрос про пол
+                Assert.IsTrue(sexFormQuestion.Displayed, "Ой-ой, вопрос про пол не отображается");
+                Assert.AreEqual("Кто у тебя?", sexFormQuestion.Text, "Текст вопроса некорректен");
+
+                //Проверка текста про почту
+                Assert.IsTrue(emailFormQuestion.Displayed, "Ой-ой, про почту про почту не отображается");
+                Assert.AreEqual("На какой e-mail прислать варианты имён?", emailFormQuestion.Text,
+                    "Текст вопроса некорректен");
+
+                //Проверка текста про пол
+                Assert.IsTrue(resultText.Text.Contains("вашего мальчика"),
+                    "Поле содержит некорректный пол, некорректно определяется скриптом");
+
+                //Проверека введённого значения почты
+                Assert.IsTrue(yourEmail.Displayed, "Текст почты не отображается");
+                Assert.AreEqual(test_email, yourEmail.Text, "Выводимый текст почты не совпадает с введённым значением");
+
+                //Проверка гиперссылки и после нажатия "Указать другой email"
+                Assert.AreEqual("указать другой e-mail", anotherEmail.Text, "Текст гиперссылки некорректен");
+
+            });
+
+        }
+
+        [Test]
+        public void ParrotName_Female_Success()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+
+            var title = driver.FindElement(TitleLokator);
+            var subbtitle_bold = driver.FindElement(Subtitle_boldLokator);
+            var sexFormQuestion = driver.FindElement(sexFormQuestionLokator);
+            var radioButton = driver.FindElement(FemaleRadioButtonLokator);
+            var emailFormQuestion = driver.FindElement(emailFormQuestionLokator);
+            var EmailInput = driver.FindElement(emailInputLokator);
+            var SendMeName = driver.FindElement(sendMeButtonLokator);
+            var test_email = "test@mail.ru";
+
+            radioButton.Click();
+            EmailInput.SendKeys(test_email);
+            SendMeName.Click();
+
+            var resultText = driver.FindElement(resultTextLokator);
+            var yourEmail = driver.FindElement(yourEmailLokator);
+
+            var anotherEmail = driver.FindElement(anotherEmailLokator);
+
+            Assert.Multiple(() =>
+            {
+                //Проверка заголовка
+                Assert.IsTrue(title.Displayed, "Ой-ой, заголовок не отображается");
+                Assert.AreEqual("Не знаешь как назвать?", title.Text, "Неверный подзаголовок");
+
+                //Проверка подзаголовка
+                Assert.IsTrue(subbtitle_bold.Displayed, "Ой-ой, подзаголовок не отображается");
+                Assert.AreEqual("Мы подберём имя для твоего попугайчика!", subbtitle_bold.Text,
+                    "Неверный подзаголовок");
+
+                //Проверка вопрос про пол
+                Assert.IsTrue(sexFormQuestion.Displayed, "Ой-ой, вопрос про пол не отображается");
+                Assert.AreEqual("Кто у тебя?", sexFormQuestion.Text, "Текст вопроса некорректен");
+
+                //Проверка текста про почту
+                Assert.IsTrue(emailFormQuestion.Displayed, "Ой-ой, про почту про почту не отображается");
+                Assert.AreEqual("На какой e-mail прислать варианты имён?", emailFormQuestion.Text,
+                    "Текст вопроса некорректен");
+
+                //Проверка текста про пол
+                Assert.IsTrue(resultText.Text.Contains("вашей девочки"),
+                    "Поле содержит некорректный пол, некорректно определяется скриптом");
+
+                //Проверека введённого значения почты
+                Assert.IsTrue(yourEmail.Displayed, "Текст почты не отображается");
+                Assert.AreEqual(test_email, yourEmail.Text, "Выводимый текст почты не совпадает с введённым значением");
+
+                //Проверка гиперссылки и после нажатия "Указать другой email"
+                Assert.AreEqual("указать другой e-mail", anotherEmail.Text, "Текст гиперссылки некорректен");
+
+            });
+        }
+
+        [Test]
+        public void ParrotName_Male_AnotherEmail_Success()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+
+            var radioButton = driver.FindElement(MaleRadioButtonLokator);
+            var EmailInput = driver.FindElement(emailInputLokator);
+            var SendMeName = driver.FindElement(sendMeButtonLokator);
+            var test_email = "test@mail.ru";
+            var resultTextBlock = 0;
+
+            radioButton.Click();
+            EmailInput.SendKeys(test_email);
+            SendMeName.Click();
+
+            var anotherEmail = driver.FindElement(anotherEmailLokator);
+
+            anotherEmail.Click();
+
+            try
+            {
+                driver.FindElement(resultTextBlockLokator);
+            }
+            catch
+            {
+                resultTextBlock = 0;
+            }
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsEmpty(EmailInput.Text, "Поле Email должно очистить после нажатия гиперссылки");
+                Assert.IsFalse(anotherEmail.Displayed, "Гиперссылка 'указать другой email' не должна отображаться");
+                Assert.AreEqual(0, resultTextBlock, "Инфомрационное поле об отправке должно быть удалено");
+
+            });
+        }
+
+        [Test]
+        public void ParrotName_Female_AnotherEmail_Success()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+
+            var radioButton = driver.FindElement(FemaleRadioButtonLokator);
+            var EmailInput = driver.FindElement(emailInputLokator);
+            var SendMeName = driver.FindElement(sendMeButtonLokator);
+            var test_email = "test@mail.ru";
+            var resultTextBlock = 0;
+
+            radioButton.Click();
+            EmailInput.SendKeys(test_email);
+            SendMeName.Click();
+
+            var anotherEmail = driver.FindElement(anotherEmailLokator);
+
+            anotherEmail.Click();
+
+            try
+            {
+                driver.FindElement(resultTextBlockLokator);
+            }
+            catch
+            {
+                resultTextBlock = 0;
+            }
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsEmpty(EmailInput.Text, "Поле Email должно очистить после нажатия гиперссылки");
+                Assert.IsFalse(anotherEmail.Displayed, "Гиперссылка 'указать другой email' не должна отображаться");
+                Assert.AreEqual(0, resultTextBlock, "Инфомрационное поле об отправке должно быть удалено");
+
+            });
+        }
+
+        [Test]
+        public void MaleEmptyEmailGetError()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+            
+            var radioButton = driver.FindElement(MaleRadioButtonLokator);
+            var SendMeName = driver.FindElement(sendMeButtonLokator);
+            
+            radioButton.Click();
+            SendMeName.Click();
+            
+            var EmailInput = driver.FindElement(emailInputLokator);
+            var getErrorClass = EmailInput.GetAttribute("class");
+            var getCSSerrorClassBorder = driver.FindElement(emailInputLokator).GetCssValue("border");
+            var formError = driver.FindElement(formErrorLokator);
+
+            Assert.Multiple((() =>
+                    {
+                        Assert.IsTrue(EmailInput.Displayed, "Поле для ввода Email не отображается");
+                        Assert.AreEqual("error", getErrorClass, "В поле Email не отобразился class=error");
+                        Assert.AreEqual("1px solid rgb(217, 21, 21)", getCSSerrorClassBorder, "Некорректное CSS форматирование границы поля Email при ошибке");
+                        Assert.IsTrue(formError.Displayed, "Текст ошибки не отобразился");
+                        Assert.AreEqual("Введите email", formError.Text, "Текст ошибки не соответсвует само ошибке");
+                    }
+                    ));
+            
+        }
+        
+        [Test]
+        public void FemaleEmptyEmailGetError()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+            
+            var radioButton = driver.FindElement(FemaleRadioButtonLokator);
+            var SendMeName = driver.FindElement(sendMeButtonLokator);
+            
+            radioButton.Click();
+            SendMeName.Click();
+            
+            var EmailInput = driver.FindElement(emailInputLokator);
+            var getErrorClass = EmailInput.GetAttribute("class");
+            var getCSSerrorClassBorder = driver.FindElement(emailInputLokator).GetCssValue("border");
+            var formError = driver.FindElement(formErrorLokator);
+
+            Assert.Multiple((() =>
+                    {
+                        Assert.IsTrue(EmailInput.Displayed, "Поле для ввода Email не отображается");
+                        Assert.AreEqual("error", getErrorClass, "В поле Email не отобразился class=error");
+                        Assert.AreEqual("1px solid rgb(217, 21, 21)", getCSSerrorClassBorder, "Некорректное CSS форматирование границы поля Email при ошибке");
+                        Assert.IsTrue(formError.Displayed, "Текст ошибки не отобразился");
+                        Assert.AreEqual("Введите email", formError.Text, "Текст ошибки не соответсвует само ошибке");
+                    }
+                ));
+            
+        }
+
+        [Test]
+        public void IncorrectEmailGetError_Success()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/selenium-practice/");
+            
+            var EmailInput = driver.FindElement(emailInputLokator);
+            var SendMeName = driver.FindElement(sendMeButtonLokator);
+            string inocorrect_test_email = "билиберда137@bilberda731.точкару";
+            
+            EmailInput.SendKeys(inocorrect_test_email);
+            SendMeName.Click();
+
+            var getCSSerrorClassBorder = driver.FindElement(emailInputLokator).GetCssValue("border");
+            var formError = driver.FindElement(formErrorLokator);
+            
+            Assert.Multiple(() =>
+                {
+                    Assert.AreEqual("1px solid rgb(217, 21, 21)", getCSSerrorClassBorder, "Некорректное CSS форматирование границы поля Email при ошибке");
+                    Assert.IsTrue(formError.Displayed, "Поле с ошибкой не появилось");
+                    
+                }
+            );
+            
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumTests/SeleniumTests/FirstSeleniumClass.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal.Execution;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    public class FirstSeleniumTests
+    {
+// Открыть браузер
+// Перейти на страницу https://ru.wikipedia.org/
+// Ввести в поле поиска слово «Selenium»
+// Нажать кнопку поиска
+// Убедиться, что перешли на страницу про Selenium
+//Страница_Действие_ОжидаемыйРезультат
+
+        [SetUp]
+        public void SetUp()
+        {
+//открываем браузер, разворачиваем его на полный экран
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            driver = new ChromeDriver(options);
+        }
+
+
+        private ChromeDriver driver;
+        private readonly By sarchInputLocator = By.Name("search");
+
+        [Test]
+        public void Wikipedia_Search_Success()
+        {
+            driver.Navigate().GoToUrl("https://ru.wikipedia.org/");
+            var searchInput = driver.FindElement(sarchInputLocator);
+            searchInput.SendKeys("Selenium");
+
+            var searchButton = driver.FindElement(By.Name("go"));
+            searchButton.Click();
+
+            var expectedText = "Selenium — Википедия";
+            Assert.True(driver.Title.Contains($"{expectedText}"), $"Ожидали Title = '{expectedText}'\r\n" +
+                                                                  $"Получили '{driver.Title}'");
+        }
+
+        private By emailInputLokator = By.ClassName("email");
+        private By buttonWruteToMeLokator = By.Id("write-to-me");
+        private By resultTextLokator = By.Name("result-text");
+        private By resultEmailLokator = By.TagName("pre");
+        private By linkAontherEmailLolator = By.LinkText("указать другой e-mail");
+        private string urlFixPC = "https://qa-course.kontur.host/selenium-lesson/";
+
+        [Test]
+        public void FixPC_SendEmail_Success()
+        {
+            var expectedResultText = "Заявка успешно создана!!! Мы скоро вам напишем по этому e-mail:";
+//перейти по урлу
+            driver.Navigate().GoToUrl(urlFixPC);
+
+//указать алрес мыла
+            var email = "test@mail.ru";
+            driver.FindElement(emailInputLokator).SendKeys(email);
+//Нажать кнопку
+            driver.FindElement(buttonWruteToMeLokator).Click();
+//проверяем что появился текст "Зяавка принята"
+            Assert.IsTrue(driver.FindElement(resultTextLokator).Displayed,
+                "Сообщение об успехе создания заявки отображается");
+            Assert.AreEqual(expectedResultText, driver.FindElement(resultTextLokator).Text,
+                "Неверное сообщение об успехе создания заявки");
+//проверяем что емейл совпадает с тем что отправляли
+            Assert.AreEqual(email, driver.FindElement(resultEmailLokator).Text,
+                "Неверный емейл на кторый будем отвечать");
+//проверяем что появилась ссылка "указать другой емейл"
+            Assert.IsTrue(driver.FindElement(linkAontherEmailLolator).Displayed,
+                "Ссылка 'Указать другой e-mail' отображается");
+        }
+
+        [Test]
+        public void FixPC_ClickAnotherEmail_Success()
+        {
+            var expectedResultText = "Заявка успешно создана!!! Мы скоро вам напишем по этому e-mail:";
+//перейти по урлу
+            driver.Navigate().GoToUrl(urlFixPC);
+
+//указать алрес мыла
+            var email = "test@mail.ru";
+            driver.FindElement(emailInputLokator).SendKeys(email);
+//Нажать кнопку
+            driver.FindElement(buttonWruteToMeLokator).Click();
+//Кликнуть ссылку "указать другой емаил"
+            driver.FindElement(linkAontherEmailLolator).Click();
+//проверим что поле очистило
+            Assert.IsEmpty(driver.FindElement(emailInputLokator).Text, "Ожидали что поле для ввода емейла осистится");
+//проверим что кнопка появилась
+            Assert.IsTrue(driver.FindElement(buttonWruteToMeLokator).Displayed,
+                "Ожидали, что кнопка 'Наишите мне' отображается");
+//проверим что исчезда ссылка "указать другой емаил"
+            Assert.AreEqual(0, driver.FindElements(linkAontherEmailLolator).Count);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
